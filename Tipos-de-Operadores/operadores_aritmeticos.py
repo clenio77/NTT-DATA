b9d661311437254ef6b79c9a38651f6143b367b5
clenio <mouraaf@hotmail.com>
2024-08-29 20:17:20 -0300

    criando repositorio Tipos de Operadors Python

---
+++ Tipos-de-Operadores/operadores_aritmeticos.py
@@ -0,0 +1,17 @@
+produto_1 = 20
+produto_2 = 30
+
+print('SOMA ', produto_1 + produto_2)
+print('SUBTRAÇÃO ',produto_1 - produto_2)
+print('MULTIPLICAÇÃO ', produto_1 * produto_2)
+print('DIVISÃO ',produto_1 / produto_2)
+print('DIVISÃO SEM RESTO ', produto_1 // produto_2)
+print('MODULO OU RESTO ',produto_1 % produto_2)
+print('EXPONENCIAÇÃO ', produto_1 ** produto_2)
+
+
+x = (10 + 5) * 4
+y = (10 / 2) + (25 * 2) - (56 - 12)
+
+print(x)
+print(y)
