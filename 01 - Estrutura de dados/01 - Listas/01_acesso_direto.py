1529e2e402dd5d6f93f8f0fd2ca51df77829a786
clenio <mouraaf@hotmail.com>
2024-09-05 20:00:27 -0300

    subindo diretorio 01 - Estrutura de dados e seus arquivos

---
+++ 01 - Estrutura de dados/01 - Listas/01_acesso_direto.py
@@ -0,0 +1,15 @@
+frutas = ["maçã", "laranja", "uva", "pera"]
+
+print(frutas[0])  # maçã
+print(frutas[2])  # uva
+
+
+lista = ['p','y','t','h','o','n']
+
+lista[2:0] # da posição 2 pra frente
+lista[:2] # até a posição 2 exclusiva
+lista[1:3] # da posição 1 até a 3 exclusiva
+lista[0:3:2] #da posição 0 até 3, pulando de 2 em 2
+lista[::] # tudo padrão
+lista[::-1] # traz a lista ao contrario(espelhado)
+
